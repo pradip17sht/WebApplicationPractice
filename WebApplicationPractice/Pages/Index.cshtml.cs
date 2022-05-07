22438d2de428ccbe0e2c511844c71efafc33dd16
Pradip Shrestha <pradip.shrestha@amniltech.com>
2022-05-07 17:49:22 +0545

    api web application upload

---
+++ WebApplicationPractice/Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplicationPractice.Models;
+using WebApplicationPractice.Services;
+
+namespace WebApplicationPractice.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public JsonFileProductService ProductService { get; }
+        public IEnumerable<Product>? Products { get; private set; }   
+
+        public IndexModel(ILogger<IndexModel> logger,
+            JsonFileProductService productService)
+        {
+            _logger = logger;
+            ProductService = productService;
+        }
+
+        public void OnGet() => Products = ProductService.GetProducts();
+    }
+}
---
+++ WebApplicationPractice/Services/JsonFileProductService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using WebApplicationPractice.Models;
+
+namespace WebApplicationPractice.Services
+{
+    public class JsonFileProductService
+    {
+        public IWebHostEnvironment WebHostEnvironment { get; }
+        public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+        private string JsonFileName => Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json");
+
+        public IEnumerable<Product> GetProducts()
+        {
+            using (var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }  
+        }
+    }
+}
